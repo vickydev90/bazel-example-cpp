0c599b4ace3cbecd4f732841b94644638dfa0337
vishal deshmukh <vishaldeshmukh@vishals-macbook-air.home>
2020-04-07 11:18:24 +0100

    removed sub dire

--- submodule/bare.cc
+++
@@ -1,6 +0,0 @@
-#include "submodule/src/bare/bare.h"
-#include <iostream>
-
-int main(int argc, char** argv) {
-  std::cout << "Hello, " << bare::world() << std::endl;
-}
--- submodule/remote.cc
+++
@@ -1,6 +0,0 @@
-#include "submodule/src/remote/remote.h"
-#include <iostream>
-
-int main(int argc, char** argv) {
-  std::cout << "Hello, " << remote::world() << std::endl;
-}
